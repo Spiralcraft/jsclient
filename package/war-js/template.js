084dae13dafb0e1b3e16532152f31e5fa6d47dff
Mike Toth <mike.git@spiralcraft.com>
2017-08-22 16:01:51 -0700

    Update tests

--- package/war-js/template.js
+++ package/war-js/template.js
@@ -28,5 +28,38 @@ SPIRALCRAFT.template = (function(self) {
     
   });
 
+  self.expandTemplates = (function (root) {
+    SPIRALCRAFT.dom.scanNodes
+      (root
+      ,function (parent,child) {
+          var templateRef=self.readTemplateRef(child);
+          return !templateRef;
+        }
+      );
+  });
+  
+  // Determine whether a node is a template reference
+  self.readTemplateRef = function(node) {
+    
+    // Explicit data-template attribute in dom node
+    var tpName=SPIRALCRAFT.dom.attributeValue(node,"data-template");
+    if (tpName)
+    {
+      self.handleDomElementUsesTemplate(node,tpName);
+      return true;
+    }
+    
+    if (node.nodeName=="SCRIPT")
+    {
+      console.log(node.nodeName+":"+node.getAttribute("type"));
+    }
+    return null;
+    
+  };
+  
+  self.handleElementUsesTemplate = function(node,tpName) {
+    console.log("Template="+tpName+" : "+node);
+  };
+  
   return self;
 }(SPIRALCRAFT.template || {}));
